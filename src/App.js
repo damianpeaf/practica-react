dc1ad27c41522a8a67df7916da4f6c5d44de5add
Damian <damianpeaf@gmail.com>
2021-11-29 14:33:33 -0600

    Primera parte, dia 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function HelloWorld(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="hello"> Hello world </div>
   );
 }
+
+// const App = () => <div>This is my component: <HelloWorld/> </div>
+
+class App extends React.Component{
+  render(){
+    return <div>This is my component: <HelloWorld/> </div>
+  }
+}
+
+// function App() {
+//   return (
+//     <div>This is my component: <HelloWorld/> </div>
+//   );
+// }
 
 export default App;
